d04c4bb03ea96d70b9e83308b9520975421c6bd2
Kot <117119203+Lomakin41DIMOON@users.noreply.github.com>
2023-05-23 18:20:43 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang=ru dir="ltr">
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>JobFitTest -  Главная</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1 class="logo">JobFitTest</h1>
+      </div>
+    </header>
+    <nav>
+      <div class="container">
+        <div class="back-img">
+          <div class="nav-line">
+            <button type="button" name="button">
+              <img src="content/img/free-icon-exam-4797050.png" alt="">
+              <h2 class="white">Пройти тестирование</h2>
+            </button>
+            <button type="button" name="button">
+              <img src="content/img/free-icon-information-157933.png" alt="">
+              <h2 class="white">О приложении</h2>
+            </button>
+            <button type="button" name="button">
+              <img src="content/img/free-icon-open-book-166088.png" alt="">
+              <h2 class="white">Справочный материал</h2>
+            </button>
+            <button type="button" name="button">
+              <img src="content/img/free-icon-checklist-876749.png" alt="">
+              <h2 class="white">Результаты тестирования</h2>
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>

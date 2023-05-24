5456e8b28e31410d4c72874f3f304bfe5897bf2b
Kot <Lomakin41DIMOON@yandex.ru>
2023-05-24 17:18:05 +0400

    Изменения:
    - Изенения размера шрифта и толщины во всех элементах, кроме <p>
    - Логотип был доработан, теперь нету проблема между частью Job и FitTest
    - Увеличин размер контейнера до 110 vh
    - Уменьшен размер иконок, для кнопок, в блоке nav
    - Блок nav был уменьшен до 450px
    - У блоков header и main был убран тэг background-color
    - В блок body был добавлен тэг background-color со значением #fafcff
    - Замена изображения Герба ФТС на полупрозрачную версию
    
    Добавление
    
    - Блоки с классом info-line (Информация для пользователя), размещение по центру с автоцентрованием относиельно container
    
    ~ Автоцентрование относиельно горизонтали
    
    - Блоки с классом info-block (Сам блок с информацией) Внутри которого преимущества приложения
    
    ~ Автоцентрование относиельно горизонтали
    ~ Отступы между блоками 50px (спарва и слева)
    ~ Ширина 300px
    ~ Выравнивание текста по центру
    ~ Отступ между Заголовком и текстом 10px
    
    - Блоки с классом icon-block (Блок под иконку):
    
    ~ Фоновый градиент (Как в блоке nav)
    ~ Размер 80 на 80 px
    ~ Полное скругление
    ~ Автоцентрование по горизонтали
    
    - Блоки img с иконкоми размеры которых 50px с автоцентрованием по вертикальной и горизонтальной оси
    - Текст (<p>) и заголовок (<h4>) внутри каждого блока
    - Тэг h4 со размером шрифта 20px и толщиной Bold(700)
    
    Требуется
    
    - Адаптировать под мобильное устройство
    - Footer
    - Добавить Кнопку ввхода и выхода в блок header с левой стороны (кнопку выхода скрыть)
    
    //Да я долбаёб хазхазха я не смог сделать это под мобилку, да и выглядет хуёво ес честно

--- index.php
+++ index.php
@@ -1,49 +1,88 @@
 <!DOCTYPE html>
 <html lang=ru dir="ltr">
-  <head>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-    <meta charset="utf-8">
-    <title>JobFitTest -  Главная</title>
-    <link rel="stylesheet" href="css/main.css">
-  </head>
-  <body>
-    <header>
-      <div class="container">
-        <h1 class="logo">
-          <span class="yellow">Job</span>
-          <span class="green">FitTest</span>
-        </h1>
-      </div>
-    </header>
-    <nav>
-      <div class="container">
-        <div class="back-img">
-          <div class="nav-line">
-            <button type="button" name="button">
-              <img src="content/img/free-icon-exam-4797050.png" alt="">
-              <h2 class="white">Пройти тестирование</h2>
-            </button>
-            <button type="button" name="button">
-              <img src="content/img/free-icon-information-157933.png" alt="">
-              <h2 class="white">О приложении</h2>
-            </button>
-            <button type="button" name="button">
-              <img src="content/img/free-icon-open-book-166088.png" alt="">
-              <h2 class="white">Справочный материал</h2>
-            </button>
-            <button type="button" name="button">
-              <img src="content/img/free-icon-checklist-876749.png" alt="">
-              <h2 class="white">Результаты тестирования</h2>
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <main>
 
-    </main>
-    <footer>
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<title>JobFitTest - Главная</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
 
-    </footer>
-  </body>
+<body>
+	<header>
+		<div class="container">
+			<h1 class="logo">
+				<span class="yellow">Job</span><span class="green">FitTest</span>
+			</h1>
+		</div>
+	</header>
+	<nav>
+		<div class="container">
+			<div class="back-img">
+				<div class="nav-line">
+					<button type="button" name="button">
+						<img src="content/img/free-icon-exam-4797050.png" alt="">
+						<h2 class="white">Пройти тестирование</h2>
+					</button>
+					<button type="button" name="button">
+						<img src="content/img/free-icon-information-157933.png" alt="">
+						<h2 class="white">О приложении</h2>
+					</button>
+					<button type="button" name="button">
+						<img src="content/img/free-icon-open-book-166088.png" alt="">
+						<h2 class="white">Справочный материал</h2>
+					</button>
+					<button type="button" name="button">
+						<img src="content/img/free-icon-checklist-876749.png" alt="">
+						<h2 class="white">Результаты тестирования</h2>
+					</button>
+				</div>
+				<h3 class="title white">Продемонстрируй свой профессиональный 
+					уровень с помощью тестирования</h3>
+			</div>
+		</div>
+	</nav>
+	<main>
+		<div class="container">
+			<div class="info-line">
+				<div class="info-block">
+					<div class="icon-block">
+						<img src="content/img/free-icon-efficiency-3120036.png" alt="">
+					</div>
+					<h4 class="green bold">Увеличение эффективности:</h4>
+					<p>Тестирование позволяет определить 
+						уровень знаний и навыков сотрудников,
+						 что может помочь в развитии индивидуальных
+						  программ обучения и повышении эффективности
+						   работы организации в целом.</p>
+				</div>
+				<div class="info-block">
+					<div class="icon-block">
+						<img src="content/img/free-icon-time-management-4534831.png" alt="">
+					</div>
+					<h4 class="green">Экономия времени и ресурсов:</h4>
+					<p>Веб-приложение позволяет сотрудникам проходить
+						 тестирование в любое удобное время и место,
+						  не требуя дополнительных ресурсов для
+						   организации тестирования.</p>
+				</div>
+				<div class="info-block">
+					<div class="icon-block">
+						<img src="content/img/free-icon-thumbs-up-4425133.png" alt="">
+					</div>
+					<h4 class="green">Удобство и легкость использования:</h4>
+					<p>Веб-приложение может быть разработано с учетом 
+						удобства пользователя и обеспечивать легкий 
+						доступ к необходимым материалам и инструкциям.</p>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer>
+
+	</footer>
+</body>
+
 </html>

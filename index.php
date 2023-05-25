7c4b8fcfd93ab6354e30d9a59b39c5f9410a86fb
Kot <Lomakin41DIMOON@yandex.ru>
2023-05-25 12:19:26 +0400

    Footer
    Изменения:
    - Убраны некоторые не нужные стили у класса logo
    
    Добавление:
    - Создан footer и внесенны некоторые правки:
    ~ Сплошной цвет был заменён на градиент
    
    В остальном footer соответсвует маету в Photoshop
    
    Необходимые доработки:
    - Кнопка входа
    - Кнопка выхода
    - Бургер меню (кнопка)
    - Бургер меню (блок)
    - Адаптация под мобильные устройства

--- index.php
+++ index.php
@@ -13,9 +13,12 @@
 <body>
 	<header>
 		<div class="container">
-			<h1 class="logo">
-				<span class="yellow">Job</span><span class="green">FitTest</span>
-			</h1>
+			<div class="">
+				<h1 class="logo">
+					<span class="yellow">Job</span><span class="green">FitTest</span>
+				</h1>
+				<button class="auth">Войти</button>
+			</div>
 		</div>
 	</header>
 	<nav>
@@ -81,8 +84,19 @@
 		</div>
 	</main>
 	<footer>
-
+		<div class="container">
+			<div class="footer-line">
+				<h1 class="logo">
+					<span class="yellow">Job</span><span class="white">FitTest</span>
+				</h1>
+				<div class="nav-block">
+					<h2 class="white">Пройти тестирование</h2>
+					<h2 class="white">О приложении</h2>
+					<h2 class="white">Справочный материал</h2>
+					<h2 class="white">Результаты тестирования</h2>
+				</div>
+			</div>
+		</div>
 	</footer>
 </body>
-
 </html>

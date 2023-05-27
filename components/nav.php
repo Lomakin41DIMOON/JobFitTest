e43ef7318c50b560ae1a3662760768b22e55dbd0
Kot <117119203+Lomakin41DIMOON@users.noreply.github.com>
2023-05-27 14:55:52 +0300

    Крохотные правки
    Изменения:
    - Исправлен визуальный баг, связанный с не пропорциональным размером иконки "Справочный материал" в блоке nav_line. Был добавлен div с классом nav_icon с размерам 25x25 px.
    - Изменён размер блока <a> (logo) и были убран размер блока logo

--- components/nav.php
+++ components/nav.php
@@ -2,23 +2,31 @@
 	<div class="back_img">
 		<div class="nav_line">
 			<a href="tests.php">
-				<img src="content/img/free-icon-exam-4797050.png" alt="Иконка пройти тестирование">
+				<div class="nav_icon">
+					<img src="content/img/free-icon-exam-4797050.png" alt="Иконка пройти тестирование">
+				</div>
 				<h2 class="white">Пройти тестирование</h2>
 			</a>
 			<a href="about.php">
-				<img src="content/img/free-icon-information-157933.png" alt="Иконка о приложении">
+				<div class="nav_icon">
+					<img src="content/img/free-icon-information-157933.png" alt="Иконка о приложении">
+				</div>
 				<h2 class="white">О приложении</h2>
 			</a>
 			<a href="info.php">
-				<img src="content/img/free-icon-open-book-166088.png" alt="Иконка справочный материал">
+				<div class="nav_icon">
+					<img src="content/img/free-icon-open-book-166088.png" alt="Иконка справочный материал">
+				</div>
 				<h2 class="white">Справочный материал</h2>
 			</a>
 			<a href="result.php">
-				<img src="content/img/free-icon-checklist-876749.png" alt="Иконка результаты тестирования">
+				<div class="nav_icon">
+					<img src="content/img/free-icon-checklist-876749.png" alt="Иконка результаты тестирования">
+				</div>
 				<h2 class="white">Результаты тестирования</h2>
 			</a>
-		</div>			
-		<h3 class="title white">Продемонстрируй свой профессиональный 
-				                уровень с помощью тестирования</h3>
+		</div>
+		<h3 class="title white">Продемонстрируй свой профессиональный
+			уровень с помощью тестирования</h3>
 	</div>
 </div>

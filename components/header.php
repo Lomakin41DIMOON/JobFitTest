1a38f5a06a41331a66a2e4554bee2efa5b67989f
Fmeech2 <83023261+Fmeech2@users.noreply.github.com>
2023-05-28 09:30:44 +0400

    Первая страница мобильной адаптации
    мобильная адаптация для страницы index.php, которая скорее всего будет работать и для других страниц после минимальных изменений.

--- components/header.php
+++ components/header.php
@@ -1,6 +1,19 @@
-<div class="container">
+<div class="container heder_mobile">
+	<div class="burger" id="burger">
+		<samp></samp>
+		<samp></samp>
+		<samp></samp>
+	</div>
 	<div class="logo">
 		<a href="index.php"><span class="yellow">Job</span><span class="green">FitTest</span></a>
 	</div>
-	<button class="auth">Войти</button>
+	<button class="auth cover">
+			<img src="content/icons/login1.png" alt="Вход">
+	</button>
 </div>
+<script>
+    $(".burger").click(function(){
+        $("nav").toggleClass("nav_bar_YES");
+        $(".burger").toggleClass("burgerClose");
+    });
+</script>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
 	<?php
-		require  'components/head.php'
+	require  'components/head.php'
 	?>
 	<title>JobFitTest - Главная</title>
 </head>
@@ -11,54 +11,59 @@
 <body>
 	<header>
 		<?php
-			require  'components/header.php'
+		require  'components/header.php'
 		?>
 	</header>
-	<nav>
-		<?php
-			require  'components/nav.php'
-		?>
-	</nav>
+
+	<?php require  'components/nav.php'	?>
+
 	<main>
-		<div class="container">
+		<div class="container container_mobile">
 			<div class="info_line">
 				<div class="info_block">
-					<div class="icon_block">
-						<img src="content/img/free-icon-efficiency-3120036.png" alt="">
+					<div class="icon_block ">
+						<div class="cover">
+							<img src="content/img/free-icon-efficiency-3120036.png" alt="">
+						</div>
 					</div>
-					<h4 class="green">Увеличение эффективности:</h4>
-					<p>Тестирование позволяет определить 
+					<h4 class="green ">Увеличение эффективности:</h4>
+					<p>Тестирование позволяет определить
 						уровень знаний и навыков сотрудников,
-						 что может помочь в развитии индивидуальных
-						  программ обучения и повышении эффективности
-						   работы организации в целом.</p>
+						что может помочь в развитии индивидуальных
+						программ обучения и повышении эффективности
+						работы организации в целом.</p>
 				</div>
 				<div class="info_block">
-					<div class="icon_block">
-						<img src="content/img/free-icon-time-management-4534831.png" alt="">
+					<div class="icon_block ">
+						<div class="cover">
+							<img src="content/img/free-icon-time-management-4534831.png" alt="">
+						</div>
 					</div>
 					<h4 class="green">Экономия времени и ресурсов:</h4>
 					<p>Веб-приложение позволяет сотрудникам проходить
-						 тестирование в любое удобное время и место,
-						  не требуя дополнительных ресурсов для
-						   организации тестирования.</p>
+						тестирование в любое удобное время и место,
+						не требуя дополнительных ресурсов для
+						организации тестирования.</p>
 				</div>
 				<div class="info_block">
-					<div class="icon_block">
-						<img src="content/img/free-icon-thumbs-up-4425133.png" alt="">
+					<div class="icon_block ">
+						<div class="cover">
+							<img src="content/img/free-icon-thumbs-up-4425133.png" alt="">
+						</div>
 					</div>
 					<h4 class="green">Удобство и легкость использования:</h4>
-					<p>Веб-приложение может быть разработано с учетом 
-						удобства пользователя и обеспечивать легкий 
+					<p>Веб-приложение может быть разработано с учетом
+						удобства пользователя и обеспечивать легкий
 						доступ к необходимым материалам и инструкциям.</p>
 				</div>
 			</div>
 		</div>
 	</main>
-	<footer>
+
 		<?php
-			require  'components/footer.php'
+		require  'components/footer.php'
 		?>
-	</footer>
+
 </body>
+
 </html>
